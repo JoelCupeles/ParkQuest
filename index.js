5f1276d194c2cf6b37b23970b651f719ced2e94c
JoelCupeles <joel.cupeles1998@gmail.com>
2023-03-20 20:14:31 -0500

    JS fucntion for the weather API

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-var apiKey = "https://developer.nps.gov/api/v1/parks?stateCode=MO&api_key=kUHjUZHYRN3jl4bTe4JwPdZmF3hlpYeIe0rOVhbW";
+var API_KEY_NPS = "kUHjUZHYRN3jl4bTe4JwPdZmF3hlpYeIe0rOVhbW";
+var API_KEY_WEATHER = "f461f184bb0f987cdbcdefa61ead2cc9";
+
+document.getElementById("search-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var location = document.getElementById("location").value;
+
+  getWeather(location);
+  getParks(location);
+});
+
+function getWeather(location) {
+  var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY_WEATHER}&units=imperial`;
+
+  fetch(weatherUrl)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("Failed to fetch weather data");
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      displayWeatherInfo(data);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+}
+
+function displayWeatherInfo(data) {
+  const currentWeather = document.getElementById("current-weather");
+  const forecast = document.getElementById("forecast");
+
+  currentWeather.innerText = `Current Weather: ${data.weather[0].main}, ${data.main.temp}°F`;
+  forecast.innerText = `Forecast: ${data.main.temp_min}°F - ${data.main.temp_max}°F`;
+}
