d0a6229a723cfdaeb2d4bcd336a04f2fcbb6e00e
JoelCupeles <joel.cupeles1998@gmail.com>
2023-03-20 21:36:52 -0500

    my park version js

--- index.js
+++ index.js
@@ -34,3 +34,31 @@ function displayWeatherInfo(data) {
   currentWeather.innerText = `Current Weather: ${data.weather[0].main}, ${data.main.temp}°F`;
   forecast.innerText = `Forecast: ${data.main.temp_min}°F - ${data.main.temp_max}°F`;
 }
+function getParks(location) {
+  var parkUrl = `https://developer.nps.gov/api/v1/parks?q=${location}&api_key=${API_KEY_NPS}`;
+
+  fetch(parkUrl)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("Failed to fetch park data");
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      displayParkInfo(data.data[0]);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+}
+
+function displayParkInfo(park) {
+  var parkImage = document.getElementById("park-image");
+  var parkFacts = document.getElementById("park-facts");
+  var parkDirections = document.getElementById("park-directions");
+
+  parkImage.src = park.images[0].url;
+  parkImage.alt = park.images[0].altText;
+  parkFacts.innerText = park.description;
+  parkDirections.innerText = `Directions: ${park.directionsInfo}`;
+}
